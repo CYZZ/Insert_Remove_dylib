85c5fd03b6dffcd943866eb0a345939640c4a64b
LoveSVN <408477779@qq.com>
2019-05-22 15:35:26 +0800

    增加 -w 参数 ，允许LC_LOAD_WEAK_DYLIB link 动态库或者framework.支持@rpath和@loader_path前缀。没有则添加@executable_path

--- insert_remove_dylib/Insert_dylib/Insert_dylib.m
+++ insert_remove_dylib/Insert_dylib/Insert_dylib.m
@@ -17,6 +17,11 @@
 
 +(void)Insert_dylib:(NSString *)dylib_path targetFile:(NSString *)target_path{
 
+    [Insert_dylib Insert_dylib:dylib_path targetFile:target_path insertStyle:LC_LOAD_DYLIB];
+}
+
++(void)Insert_dylib:(NSString *)dylib_path targetFile:(NSString *)target_path insertStyle:(uint32_t)insertStyle{
+    
     NSFileManager *file_manager = [NSFileManager defaultManager];
     if (![file_manager fileExistsAtPath:target_path isDirectory:NO]) {
         fprintf(stderr, "error:target file not exist!!\n");
@@ -24,7 +29,7 @@
         return;
     }
     
-    if ([dylib_path hasPrefix:@"@executable_path/"]) {
+    if ([dylib_path hasPrefix:@"@executable_path/"] || [dylib_path hasPrefix:@"@rpath/"] || [dylib_path hasPrefix:@"@loader_path/"]) {
         
     }else{
         dylib_path = [NSString stringWithFormat:@"@executable_path/%@",dylib_path];
@@ -54,9 +59,9 @@
             for (i=0; i<CFSwapInt32(fh->nfat_arch); i++) {
                 fprintf(stderr, "Injecting to arch %i\n",CFSwapInt32(arch->cpusubtype));
                 if (CFSwapInt32(arch->cputype) == CPU_TYPE_ARM64) {
-                    [self inject_64:target_fp startAddress:CFSwapInt32(arch->offset) injectPath:dylib_path];
+                    [self inject_64:target_fp startAddress:CFSwapInt32(arch->offset) injectPath:dylib_path insertStyle:insertStyle];
                 }else{
-                    [self inject_32:target_fp startAddress:CFSwapInt32(arch->offset) injectPath:dylib_path];
+                    [self inject_32:target_fp startAddress:CFSwapInt32(arch->offset) injectPath:dylib_path insertStyle:insertStyle];
                 }
                 arch++;
             }
@@ -66,14 +71,14 @@
         case MH_MAGIC_64:
         {
             fprintf(stderr, "Thin 64bit binary!\n");
-            [self inject_64:target_fp startAddress:0 injectPath:dylib_path];
+            [self inject_64:target_fp startAddress:0 injectPath:dylib_path insertStyle:insertStyle];
         }
             break;
         case MH_CIGAM:
         case MH_MAGIC:
         {
             fprintf(stderr, "Thin 32bit binary!\n");
-            [self inject_32:target_fp startAddress:0 injectPath:dylib_path];
+            [self inject_32:target_fp startAddress:0 injectPath:dylib_path insertStyle:insertStyle];
         }
             break;
         default:
@@ -87,7 +92,8 @@
 
 
 /***************************************************/
-+(void)inject_64:(FILE *)fp startAddress:(unsigned int)top injectPath:(NSString *)dylib_path{
+
++(void)inject_64:(FILE *)fp startAddress:(unsigned int)top injectPath:(NSString *)dylib_path insertStyle:(uint32_t) insertStyle{
     //将指针指向64架构的开始位置
     fseek(fp, top, SEEK_SET);
     
@@ -100,10 +106,10 @@
     char orig_dyld_name[1024];
     //遍历所有的load command 段，查看是否已经注入相同的动态库，如果已经注入相同的库，就不注入了
     for (int i=0; i<mach.ncmds; i++) {
-
+        
         fread(&exist_cmd, sizeof(struct load_command), 1, fp);
-
-        if (exist_cmd.cmd == LC_LOAD_DYLIB) {
+        
+        if (exist_cmd.cmd == insertStyle) {
             fseek(fp, -sizeof(struct load_command), SEEK_CUR);
             fread(&orig_dyld, sizeof(struct dylib_command), 1, fp);
             fseek(fp, orig_dyld.dylib.name.offset - sizeof(struct dylib_command), SEEK_CUR);
@@ -121,7 +127,7 @@
     }
     //将所有的段都读完了，没有发现重复的，将指针指向load command 开始的地方
     fseek(fp, -(unsigned long)mach.sizeofcmds, SEEK_CUR);
-
+    
     NSData *data = [dylib_path dataUsingEncoding:NSUTF8StringEncoding];
     
     //计算注入的load command 段的大小(cmd大小是 long 对齐的)
@@ -154,7 +160,7 @@
     fread(&dyld, sizeof(struct dylib_command), 1, fp);
     
     //给新的cmd赋值
-    dyld.cmd = LC_LOAD_DYLIB;
+    dyld.cmd = insertStyle;
     dyld.cmdsize = (uint32_t)new_cmd_size;
     dyld.dylib.compatibility_version = DYLIB_COMPATIBILITY_VERSION;
     dyld.dylib.current_version = DYLIB_CURRENT_VER;
@@ -168,14 +174,12 @@
     fwrite([data bytes], [data length], 1, fp);
 }
 
-
-
 /***************************************************/
 
 
 
 /***************************************************/
-+(void)inject_32:(FILE *)fp startAddress:(unsigned int)top injectPath:(NSString *)dylib_path{
++(void)inject_32:(FILE *)fp startAddress:(unsigned int)top injectPath:(NSString *)dylib_path insertStyle:(uint32_t) insertStyle{
     fseek(fp, top, SEEK_SET);
     struct mach_header mach;
     fread(&mach, sizeof(struct mach_header), 1, fp);
@@ -188,7 +192,7 @@
     for (int i=0; i<mach.ncmds; i++) {
         
         fread(&exist_cmd, sizeof(struct load_command), 1, fp);        
-        if (exist_cmd.cmd == LC_LOAD_DYLIB) {
+        if (exist_cmd.cmd == insertStyle) {
             fseek(fp, -sizeof(struct load_command), SEEK_CUR);
             fread(&orig_dyld, sizeof(struct dylib_command), 1, fp);
             fseek(fp, orig_dyld.dylib.name.offset - sizeof(struct dylib_command), SEEK_CUR);
@@ -232,7 +236,7 @@
     fread(&dyld, sizeof(struct dylib_command), 1, fp);
     
     //给新的cmd赋值
-    dyld.cmd = LC_LOAD_DYLIB;
+    dyld.cmd = insertStyle;
     dyld.cmdsize = (uint32_t)dylib_size;
     dyld.dylib.compatibility_version = DYLIB_COMPATIBILITY_VERSION;
     dyld.dylib.current_version = DYLIB_CURRENT_VER;
--- insert_remove_dylib/main.m
+++ insert_remove_dylib/main.m
@@ -10,13 +10,14 @@
 #import <getopt.h>
 #import "Insert_dylib/Insert_dylib.h"
 #import "Remove_dylib/Remove_dylib.h"
-
+#import <mach-o/loader.h>
 void print_usage(){
     fprintf(stderr, "insert_remove_dylib \n"
             "Usage: insert_remove_dylib [options] <dylib_path> <mach-o-file>\n"
             "\n"
             "   where options are:\n"
-            "       -i          inject a dylib to mach-o file\n"
+            "       -i          inject a dylib to mach-o file by LC_LOAD_DYLIB\n"
+            "       -w          with -i mean by  inject a dylib to mach-o file LC_LOAD_WEAK_DYLIB\n"
             "       -r          remove a dylib in mach-o file\n");
 }
 
@@ -33,23 +34,32 @@ int main(int argc, const char * argv[]) {
         
         struct option longopts[] = {
             {"insert-dylib",        required_argument,      NULL,   'i'},
+            {"insert-dylib",        optional_argument,      NULL,   'w'},
             {"remove-dylib",        required_argument,      NULL,   'r'},
             {NULL,                  0,                      NULL,   0},
             
         };
         int ch;
         BOOL errorFlag = NO;
-        while ((ch = getopt_long(argc, (char * const *)argv, "i:r:", longopts, NULL)) != -1) {
+        int operation;
+        while ((ch = getopt_long(argc, (char * const *)argv, "i:w:r:", longopts, NULL)) != -1) {
             switch (ch) {
                 case 'i':
                 {
                     //获取注入的路径
                     insert_dylib_path = [NSString stringWithUTF8String:optarg];
+                    operation = 'i';
+                    if (strcmp(optarg, "-w") == 0) {
+                        insert_dylib_path = [NSString stringWithUTF8String:argv[optind]];
+                        operation = 'w';
+                    }
+                    
                 }
                     break;
                 case 'r':
                 {
                     remove_dylib_path = [NSString stringWithUTF8String:optarg];
+                    operation = 'r';
                 }
                     break;
                 case 0:
@@ -69,13 +79,19 @@ int main(int argc, const char * argv[]) {
             //获取target文件路径
             target_path = [NSString stringWithUTF8String:argv[optind]];
             
-            if (insert_dylib_path) {
+            if (insert_dylib_path && operation == 'i') {
                 //注入
                 fprintf(stderr, "insert:%s\n",[insert_dylib_path UTF8String]);
                 
                 [Insert_dylib Insert_dylib:insert_dylib_path targetFile:target_path];
             }
-            if (remove_dylib_path) {
+            if (insert_dylib_path && operation == 'w') {
+                //注入
+                fprintf(stderr, "insert:%s\n",[insert_dylib_path UTF8String]);
+                target_path = [NSString stringWithUTF8String:argv[optind + 1]];
+                [Insert_dylib Insert_dylib:insert_dylib_path targetFile:target_path insertStyle:LC_LOAD_WEAK_DYLIB];
+            }
+            if (remove_dylib_path && operation == 'r') {
                 //移除
                 fprintf(stderr, "remove:%s\n",[remove_dylib_path UTF8String]);
                 [Remove_dylib Remove_dylib:remove_dylib_path targetFile:target_path];
